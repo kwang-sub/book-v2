8e8850e85549ba5e7d57396d7ba575ac171666ff
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2023-02-21 22:15:57 +0900

    기본 문법 학습(enum, when)

--- kotlin-in-action/src/main/kotlin/ch02/Ex02.kt
+++ kotlin-in-action/src/main/kotlin/ch02/Ex02.kt
@@ -1,7 +1,15 @@
 package ch02
 
+import geometry.shapes.createRandomRectangle
+
 fun main() {
     val person  = Person("Bob", true)
     println(person.name)
     println(person.isMarried)
+
+    val rectangle = createRandomRectangle()
+    println(rectangle.isSquare)
+
+    println(Color.INDIGO.rgb())
+    println(getMnemonic(Color.VIOLET))
 }
