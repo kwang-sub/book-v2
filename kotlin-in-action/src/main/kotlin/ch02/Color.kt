55ae914a6063692797919ef5d2f9e060d3af0620
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2023-02-27 23:02:59 +0900

    기본 문법 학습(when)

--- kotlin-in-action/src/main/kotlin/ch02/Color.kt
+++ kotlin-in-action/src/main/kotlin/ch02/Color.kt
@@ -21,3 +21,28 @@ fun getMnemonic(color: Color) =
         INDIGO -> "In"
         VIOLET -> "Vain"
     }
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, YELLOW) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == RED && c2 == BLUE) ||
+                (c1 == YELLOW && c2 == RED) ->
+            ORANGE
+
+        (c1 == YELLOW && c2 == BLUE) ||
+                (c1 == BLUE && c2 == YELLOW) ->
+            GREEN
+
+        (c1 == BLUE && c2 == VIOLET) ||
+                (c1 == VIOLET && c2 == BLUE) ->
+            INDIGO
+
+        else -> throw Exception("Dirty color")
+    }
--- kotlin-in-action/src/main/kotlin/ch02/Ex02.kt
+++ kotlin-in-action/src/main/kotlin/ch02/Ex02.kt
@@ -1,15 +1,26 @@
 package ch02
 
+import ch02.Color.*
 import geometry.shapes.createRandomRectangle
 
 fun main() {
     val person  = Person("Bob", true)
     println(person.name)
     println(person.isMarried)
+    println()
 
     val rectangle = createRandomRectangle()
     println(rectangle.isSquare)
+    println()
 
-    println(Color.INDIGO.rgb())
-    println(getMnemonic(Color.VIOLET))
+    println(INDIGO.rgb())
+    println(getMnemonic(VIOLET))
+    println()
+
+    println(mix(BLUE, YELLOW))
+    println(mix(YELLOW, BLUE))
+    println()
+
+    println(mixOptimized(BLUE, YELLOW))
+    println(mixOptimized(BLUE, BLUE))
 }
