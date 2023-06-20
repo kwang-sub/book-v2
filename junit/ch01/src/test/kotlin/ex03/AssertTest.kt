77a0f0c652bc39b64c1d4d76c20c9f1c5d170fbb
kwang <choikwangsub@gmail.com>
2023-06-20 20:53:46 +0900

    add: 예외처리

--- junit/ch01/src/test/kotlin/ex03/AssertTest.kt
+++ junit/ch01/src/test/kotlin/ex03/AssertTest.kt
@@ -1,13 +1,18 @@
 package ex03
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.concurrent.ExecutionException
+import javax.naming.InsufficientResourcesException
 
 class AssertTest {
 
     private lateinit var str: String
+
+
     @BeforeEach
     fun init() {
         str = "kwang"
@@ -29,6 +34,24 @@ class AssertTest {
 
         assertNotEquals(2.32 * 3, 6.96)
         assertEquals(Math.abs((2.32 * 3) - 6.96) < 0.0005, true)
-        assertEquals(1, 1,"1은 1이다")
+        assertEquals(1, 1, "1은 1이다")
+
+        assertThrows(RuntimeException::class.java) { throwRuntime() }
+
+
+    }
+
+
+    @Test
+    fun throwTest() {
+        try {
+            throwRuntime()
+            fail()
+        } catch (run: RuntimeException) {
+            assertEquals(run::class.java, RuntimeException::class.java)
+        }
+    }
+    fun throwRuntime() {
+        throw RuntimeException()
     }
 }
