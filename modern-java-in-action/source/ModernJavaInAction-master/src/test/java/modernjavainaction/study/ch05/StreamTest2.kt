1f96e8fb8a8fd6fa8b4830debce92770949b5e61
kwang-sub <74045087+kwang-sub@users.noreply.github.com>
2023-11-09 20:00:20 +0900

    add: 모던자바인액션 학습
    Resolves: https://velog.io/@kwangsub/%EC%9E%90%EB%B0%94-%EC%8A%A4%ED%8A%B8%EB%A6%BC
    
    See Also:

--- modern-java-in-action/source/ModernJavaInAction-master/src/test/java/modernjavainaction/study/ch05/StreamTest2.kt
+++ modern-java-in-action/source/ModernJavaInAction-master/src/test/java/modernjavainaction/study/ch05/StreamTest2.kt
@@ -1,8 +1,12 @@
 package modernjavainaction.study.ch05
 
+import modernjavainaction.base.chap04.Dish
 import modernjavainaction.study.ch04.menu
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.util.stream.IntStream
+import kotlin.math.sqrt
+import kotlin.streams.toList
 
 class StreamTest2 {
     @Test
@@ -11,5 +15,29 @@ class StreamTest2 {
         val sum = menu.sumOf { it.calories }
 
         assertThat(reduce).isEqualTo(sum)
+    }
+
+    @Test
+    fun 특화스트림_복원() {
+        val intStream = menu.stream().mapToInt(Dish::getCalories)
+        val boxed = intStream.boxed()
+
+        val maxCalories = menu.stream().mapToInt(Dish::getCalories)
+            .max().orElseGet { 0 }
+
+        val rangeClosed = IntStream.rangeClosed(1, 100).toList()
+        val range = IntStream.range(1, 100).toList()
+
+        assertThat(rangeClosed.size).isEqualTo(100)
+        assertThat(range.size).isEqualTo(99)
+    }
+
+    @Test
+    fun 피타고라스() {
+        val a = 10
+        IntStream.rangeClosed(1, 100)
+            .mapToObj { Triple(a, it, sqrt(((a * a + it * it).toDouble()))) }
+            .filter { it.third % 1 == 0.0 }
+            .forEach(System.out::println)
     }
 }
